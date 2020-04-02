c26a3d9b48634cf5c5963df766cde900381d7bab
yoga wahyu <yogawahyuy@gmail.com>
2020-04-02 10:47:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chatwithfirebase/View/StartActivity.java
@@ -0,0 +1,38 @@
+package com.example.chatwithfirebase.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.chatwithfirebase.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button login,register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        login=findViewById(R.id.btnLoginApp);
+        register=findViewById(R.id.btnRegisterApp);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this,LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this,RegisterActivity.class));
+            }
+        });
+    }
+}
